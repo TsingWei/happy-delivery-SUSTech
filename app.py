bf1f939edd63af715a71e539bb7c70edd404c08b
TsingWei <461626239@qq.com>
2019-05-13 16:04:25 +0800

    写好了一些测试页面以及准备好了静态资源

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+from flask_bootstrap import WebCDN
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.extensions['bootstrap']['cdns']['jquery'] = WebCDN(
+    '//cdnjs.cloudflare.com/ajax/libs/jquery/4.6.0/'
+)
 
+@app.route('/test')
+def test():
+    return render_template('base.html')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
